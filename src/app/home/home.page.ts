a0db21a8af32c9dbc9fc2efb31598b2555deb445
Iván L. Murillo <diskman199@gmail.com>
2021-12-02 20:15:37 -0600

    Home page implemented
    Get users and everything

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Contacto } from '../interfaces/contacto.interface';
+import { User } from '../interfaces/user.interface';
+import { ContactosService } from '../services/contactos.service';
+import { SesionesService } from '../services/sesiones.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public user : User;
+  public contactos : Contacto[] = [];
 
-  constructor() {}
+  constructor(private sesionesService   : SesionesService,
+              private alertController   : AlertController,
+              private router            : Router,
+              private contactosService  : ContactosService) {}
 
+  ngOnInit(){
+    this.user = this.sesionesService.getLoggedUser();
+    this.contactosService.contactos.subscribe(contactos => {
+      this.contactos = contactos;
+    });
+    this.contactosService.cargarContactos();
+  }
+
+  public async onLogout(){
+    console.log('Cerrando sesión');
+    await this.confirmLogout();
+  }
+
+  private async confirmLogout(){
+    await this.alertController.create({
+      header: 'Cerrar sesión',
+      message: '¿Desea cerrar su sesión?',
+      buttons: [
+        {
+          text: 'Confirmar', handler: _ => {
+            this.sesionesService.logout();
+          }
+        },
+        {
+          text: 'Cancelar'
+        }
+      ]
+    }).then(res => res.present());
+  }
+
+  public addContact() : void{
+    this.router.navigate(['new']);
+  }
 }
